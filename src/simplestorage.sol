8d0a8a8e1fa6a6c6bea8d12bbe6e48769399098a
Uk2476 <kuttkarsh028@gmail.com>
2026-02-25 06:53:47 +0530

    add variables and defined all function we are going to use in here

--- src/simplestorage.sol
+++ src/simplestorage.sol
@@ -2,5 +2,26 @@
 pragma solidity ^0.8.18;
 
 contract simplestorage {
+ uint246 setFavoriteNmber ;
+
+ struct person {
+    string name ,
+    uint256 number
+ }  ;
+
+ Person[] public person ;
+
+ mapping (string => uint256) nameToFavoriteNumber ;
+
+ function setFavoriteNumber(uint256 _favoriteNumber) public {
+
+ }
+
+ function getFavoriteNumber() public view return (uint256) {
+
+ }
+
+ function addPerson (string _name , uint256 _number) public {
     
+ }
 }

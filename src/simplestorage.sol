f064722a88f53735608b2a5ca95ae7852d4b2bb1
Uk2476 <kuttkarsh028@gmail.com>
2026-02-25 07:10:09 +0530

    wrote function content

--- src/simplestorage.sol
+++ src/simplestorage.sol
@@ -2,26 +2,27 @@
 pragma solidity ^0.8.18;
 
 contract simplestorage {
- uint246 setFavoriteNmber ;
+ uint256 setFavoriteNmber ;
 
- struct person {
-    string name ,
-    uint256 number
- }  ;
+ struct Person {
+    string name ;
+    uint256 number;
+ }  
 
  Person[] public person ;
 
  mapping (string => uint256) nameToFavoriteNumber ;
 
  function setFavoriteNumber(uint256 _favoriteNumber) public {
-
+    setFavoriteNumber = _favoriteNumber ; 
  }
 
- function getFavoriteNumber() public view return (uint256) {
-
+ function getFavoriteNumber() public view returns (uint256) {
+    return setFavoriteNumber ;
  }
 
- function addPerson (string _name , uint256 _number) public {
-    
+ function addPerson (string memory _name , uint256 _number) public {
+    person.append(Person(_name , _number));
+    nameToFavoriteNumber[_name] = _number ;
  }
 }
